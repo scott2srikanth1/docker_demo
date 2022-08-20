a1738e38756ed550d11ec08d8c2e9c3ac14a653a
Your Name <tsrikanthreddy@gmail.com>
2022-08-20 08:27:09 +0530

    init

--- src/App.js
+++ src/App.js
@@ -9,13 +9,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          sds
+          {`Hello ${process.env.REACT_APP_NAME} ff`}
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React vv
         </a>
       </header>
     </div>
